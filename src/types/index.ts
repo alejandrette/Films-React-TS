3cf343dc2a728a5d4a417f12b9420a5fb995c9f5
Alejandrette <alejandrette5117@gmail.com>
2025-03-22 12:56:01 +0100

    view tv serie or movie

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export type Films = {
   id: number;
   title: string;
   overview: string;
-  release_date: string;
+  media_type: string | undefined;
   poster_path: string;
   vote_average: number;
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect } from "react"
 import { useFilms } from "../store/store"
+import { mediaTypes } from "../data/mediaType"
 
 export function IndexPage() {
   const fetchFilmsDefault = useFilms(state => state.fetchFilmsDefault)
@@ -32,12 +33,22 @@ export function IndexPage() {
           <div className="p-5">
             <h5 className="text-2xl text-gray-900 dark:text-white font-bold mb-2 tracking-tight truncate">{film.title}</h5>
             <p className="text-gray-700 dark:text-gray-400 font-normal mb-3 line-clamp-3">{film.overview}</p>
-            <a href="#" className="bg-blue-700 rounded-lg text-center text-sm text-white dark:bg-blue-600 dark:focus:ring-blue-800 dark:hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium hover:bg-blue-800 inline-flex items-center px-3 py-2">
-              Read more
-              <svg className="h-3.5 w-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-              </svg>
-            </a>
+            <div className="flex flex-wrap justify-between">
+              <div>
+                <a href="#" className="bg-blue-700 rounded-lg text-center text-sm text-white dark:bg-blue-600 dark:focus:ring-blue-800 dark:hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium hover:bg-blue-800 inline-flex items-center px-3 py-2">
+                  Read more
+                  <svg className="h-3.5 w-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+                </a>
+              </div>
+
+              {mediaTypes.find(mediaType => mediaType.value === film.media_type) && (
+              <div className={`flex items-center px-3 py-1 rounded-lg text-white text-xs font-semibold ${film.media_type === 'movie' ? 'bg-orange-500' : 'bg-purple-500'}`}>
+                {mediaTypes.find(mediaType => mediaType.value === film.media_type)?.mediaType}
+              </div>
+              )}
+            </div>
           </div>
         </div>
       ))}
