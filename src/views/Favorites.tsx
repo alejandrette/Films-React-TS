b84ab8347fdcc76768aab2b62308a731b4b74157
Alejandrette <alejandrette5117@gmail.com>
2025-03-25 13:08:45 +0100

    show favorite

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import { useFilms } from "../store/store"
+import { Films } from "../types"
+import { Modal } from "../components/Modal"
+import { FilmCard } from "../components/FilmCard"
+
 export function Favorites() {
+  const favorites = useFilms(state => state.favorites)
+  const [selectedFilm, setSelectedFilm] = useState<Films | null>(null)
+
   return (
-    <div>
-      fav
+    <div className="flex flex-col items-center">
+      {favorites.length <= 0 ? (
+        <div className="flex items-center justify-center h-64">
+          <p className="text-lg font-semibold text-gray-600">There are no movies in favorites</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-10 my-4">
+          {favorites.map((film) => (
+            <FilmCard key={film.id} film={film} setSelectedFilm={setSelectedFilm} />
+          ))}
+        </div>
+      )}
+      <Modal film={selectedFilm} />
     </div>
   )
 }
